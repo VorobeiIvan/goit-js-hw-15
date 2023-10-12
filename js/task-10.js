53ab3603cf92408c9c2394a8f8cf0db3ea5217fc
VorobeiIvan <0675361905vi@gmail.com>
2023-10-13 01:13:34 +0300

    add the answer to task 10

--- js/task-10.js
+++ js/task-10.js
@@ -30,8 +30,39 @@
 // Створи функцію destroyBoxes(), яка очищає вміст div#boxes,
 //   у такий спосіб видаляючи всі створені елементи.
 
+const divBoxes = document.getElementById('boxes');
+const input = document.querySelector('input');
+const create = document.querySelector('[data-create]');
+const destroy = document.querySelector('[data-destroy]');
+
+create.addEventListener('click', () => {
+  createBoxes(input.value);
+});
+
+destroy.addEventListener('click', destroyBoxes);
+
+function createBoxes(amount) {
+  const number = amount;
+  const boxes = [];
+
+  for (let i = 0; i < number; i++) {
+    const box = document.createElement('div');
+    const size = 30 + i * 10;
+    box.style.width = size + 'px';
+    box.style.height = size + 'px';
+    box.style.backgroundColor = getRandomHexColor();
+    boxes.push(box);
+  }
+
+  divBoxes.append(...boxes);
+}
+
+function destroyBoxes() {
+  divBoxes.innerHTML = '';
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, '0')}`;
 }
